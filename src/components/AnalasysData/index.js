1bc5200b26d23e7edec09a4b13c3bf9f21934470
sumanborra <nxtwave@ip-10-2-79-95.ap-south-1.compute.internal>
2024-05-24 04:41:12 +0000

    GITHUB

--- src/components/AnalasysData/index.js
+++ src/components/AnalasysData/index.js
@@ -24,7 +24,7 @@ const AnalasysData = props => {
   const repositoryData = async () => {
     setApiStatusValue(apiStatus.loading)
     const response = await fetch(
-      `https://apis2.ccbp.in/gpv/profile-summary/${userName}?api_key=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`,
+      `https://apis2.ccbp.in/gpv/profile-summary/${userName}`,
     )
     const datare = await response.json()
 
